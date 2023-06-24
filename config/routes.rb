6a60fc6248fb9b3be0e96ce0ee9aa25ec85dee19
shogo313 <yscztd911@gmail.com>
2023-06-24 23:07:07 +0900

    roomsコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
   get 'homes/top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   get 'users/top', to: 'users#top'
   get 'users/account', to: 'users#account'
   resources :users
+  resources :rooms
 end
