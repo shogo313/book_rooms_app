bc5eb463d64efdd3bd1c294d953a57796989aa7e
shogo313 <yscztd911@gmail.com>
2023-06-26 00:26:39 +0900

    宿登録機能を作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,21 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(params.require(:room).permit(:name, :introduction, :charge_per_night, :detail, :address))
+    @room.user_id = current_user.id
+    if @room.save
+      flash[:notice] = "提供する宿を新規登録しました"
+      redirect_to homes_top_path
+    else
+      render :new
+    end
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,9 @@
 class Room < ApplicationRecord
+  belongs_to :user
+  
+  validates :name, presence: true
+  validates :introduction, length:{maximum: 50}
+  validates :charge_per_night, presence: true, numericality: true
+  validates :detail, presence: true, length:{maximum: 1000}
+  validates :address, presence: true, length:{maximum: 60}
 end
