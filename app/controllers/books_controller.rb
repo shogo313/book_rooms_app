938447661f01e99c0b3159e71ec9e86a9fc5b61a
shogo313 <yscztd911@gmail.com>
2023-07-11 00:09:42 +0900

    宿の予約機能を作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,38 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  
+
   def index
   end
+  
+  def new
+    @room = Room.find(params[:room_id])
+    @book = Book.new
+    @user = current_user
+  end
+
+  def confirm
+    @room = Room.find(params[:room_id])
+    @book = Book.new(book_params)
+    @book.number_of_nights = @book.calculate_number_of_nights
+    @book.payment_amount = @book.calculate_payment_amount(@room.charge_per_night)
+  end
+
+  def create
+    binding.pry
+    @book = Book.new(book_params)
+    binding.pry
+    if @book.save
+      flash[:notice] = "宿を予約しました"
+      redirect_to homes_top_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:check_in_date, :check_out_date, :number_of_nights, :number_of_people, :payment_amount, :user_id, :room_id)
+  end
+  
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  def calculate_number_of_nights
+    self.check_out_date - self.check_in_date
+  end
+
+  def calculate_payment_amount(charge_per_night)
+    charge_per_night * self.number_of_people * self.number_of_nights
+  end
+  
 end
