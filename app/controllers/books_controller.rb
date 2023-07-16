07eee503f53bfb26edf852fde49944b5df44a03f
shogo313 <yscztd911@gmail.com>
2023-07-16 19:13:51 +0900

    booksコントローラーをリファクタリング

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_room, only: [:new, :confirm, :back]
   
 
   def index
@@ -8,29 +9,24 @@ class BooksController < ApplicationController
   end
   
   def new
-    @room = Room.find(params[:room_id])
     @book = Book.new
     @user = current_user
   end
 
   def confirm
-    @room = Room.find(params[:room_id])
-    @book = Book.new(params.require(:book).permit(:check_in_date, :check_out_date, :number_of_people))
-    @book.user_id = current_user.id
-    @book.room_id = @room.id
+    @book = Book.new(book_params.merge(user_id: current_user.id, room_id: @room.id))
     @book.number_of_nights = @book.calculate_number_of_nights
     @book.payment_amount = @book.calculate_payment_amount
     render :new if @book.invalid?
   end
 
   def back
-    @room = Room.find(params[:room_id])
-    @book = Book.new(params.require(:book).permit(:check_in_date, :check_out_date, :number_of_people))
+    @book = Book.new(book_params)
     render :new
   end
 
   def create
-    @book = Book.new(book_params)
+    @book = Book.new(book_params_create)
     if @book.save
       flash[:notice] = "宿を予約しました"
       redirect_to homes_top_path
@@ -40,7 +36,15 @@ class BooksController < ApplicationController
   end
 
   private
+  def set_room
+    @room = Room.find(params[:room_id])
+  end
+
   def book_params
+    params.require(:book).permit(:check_in_date, :check_out_date, :number_of_people)
+  end
+
+  def book_params_create
     params.require(:book).permit(:check_in_date, :check_out_date, :number_of_nights, :number_of_people, :payment_amount, :user_id, :room_id)
   end
   
