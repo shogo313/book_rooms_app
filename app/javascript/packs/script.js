c6cce197448bce8a03721612d521c9fd4856d5f1
shogo313 <yscztd911@gmail.com>
2023-08-14 13:34:56 +0900

    ログイン中のみドロップダウンメニューの関数を呼び出すように変更

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,7 +1,13 @@
-$(document).on('turbolinks:load', function() {
+function dropDawnMenu() {
   const dropDawnMenu = document.getElementById('dropDawnMenu');
   const dropDawnMenu_button = document.getElementById('dropDawnMenu_button');
   dropDawnMenu_button.addEventListener('click', function(){
-      dropDawnMenu.classList.toggle('dropDawnMenu-open');
+    dropDawnMenu.classList.toggle('dropDawnMenu-open');
   });
+};
+
+$(document).on('turbolinks:load', function() {
+  if (document.getElementById('dropDawnMenu_button') !== null){
+    dropDawnMenu();
+  }
 });
